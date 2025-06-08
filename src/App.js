e24d8bf6c0b709110b06e1970b5b89c84c982bd5
101142-gulshan <101142@LT-APP101142>
2025-06-08 15:25:54 +0530

    updated files

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
 import { Navbar } from './Components/Navbar/Navbar';
+import Home from "./Pages/Home";
+import HomeCate from "./Pages/HomeCate";
+import { BrowserRouter, Routes,Route } from 'react-router-dom';
 
 function App() {
   return (
     <div>
-      <Navbar/>
+      <BrowserRouter>
+            <Navbar/>
+                <Routes>
+                  <Route path='/' element ={<Home/>}/>
+                  <Route path='/Medicine' element ={<HomeCate/>}/>
+                  <Route path='/Lab Test' element ={<HomeCate/>}/>
+                  <Route path='Contact/' element ={<HomeCate/>}/>
+
+                </Routes>
+      </BrowserRouter>
     </div>
   );
 }
-
 export default App;
---
+++ src/Pages/Medicine.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export const Medicine = () => {
+  return (
+    <div>Medicine</div>
+  )
+}
+export default Medicine;
